787f37b432a9cf9e9b0856c045e92bae69351342
bilannnn <bilan.r.27.03@gmail.com>
2021-08-07 13:18:58 +0300

    created post

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Ivan',
+        'title': 'Blog Post 1',
+        'content': 'Blog content',
+        'data_posted': '09 08 2021'
+    }
+]
 
 
 @app.route('/')
 def home():
-    return 'Hello world'
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    return 'About'
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
